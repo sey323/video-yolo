0d9596dec3b7436a52d6806456a368c807abb91d
Ichirou.S <sey323@users.noreply.github.com>
2022-10-29 01:10:39 +0900

    LineBotからSlackBotに変更 (#2)

--- VideoExtractor/__init__.py
+++
@@ -1,6 +0,0 @@
-import config
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-
-line_bot_api = LineBotApi(config.line_channel_access_token)
-handler = WebhookHandler(config.line_channel_secret)
